f8a536c30c4737d8a44dc5fc2fadf9d96fbe8462
dongya0410j <dongya0410j@gimg.tw>
2015-09-16 17:12:01 +0800

    內部訂單輸入購物金相關表

--- BLL.gigade/Dao/Impl/IBonusMasterImplDao.cs
+++ BLL.gigade/Dao/Impl/IBonusMasterImplDao.cs
@@ -18,31 +18,43 @@ namespace BLL.gigade.Dao.Impl
         bool UpdateBonusMaster(BonusMasterQuery store);
 
         /// <summary>
-        /// 向 bonus_master 裱中 添加 一筆數據  add by zhuoqin0830w 2015/08/24
+        /// 向 bonus_master 裱中 添加 一筆數據
         /// </summary>
         /// <param name="br"></param>
         /// <returns></returns>
         string AddBonusMaster(BonusMaster bm);
         /// <summary>
-        /// 根據 br.user_id 獲取 bonus_master 裱中 購物金的總和  add by zhuoqin0830w 2015/08/24
+        /// 得到 bonus_master 裱中 購物金的 總和
         /// </summary>
         /// <param name="br"></param>
         /// <returns></returns>
         int GetSumBouns(BonusRecord br);
         /// <summary>
-        /// 根據 User_id 和 時間 的倒序查詢出相應的列表  add by zhuoqin0830w 2015/08/24
+        /// 根據 User_id 和 時間 的倒序查詢出相應的列表 購物金
         /// </summary>
         /// <param name="br"></param>
         /// <returns></returns>
-        List<BonusMaster> GetBonusMasterByEndTime(BonusRecord br);
+        List<BonusMaster> GetBonusByEndTime(BonusRecord br);
         /// <summary>
-        /// 根據 master_id 修改數據   add by zhuoqin0830w 2015/08/24
+        /// 得到 bonus_master 裱中 抵用券 總和
         /// </summary>
-        /// <param name="bm"></param>
+        /// <param name="br"></param>
+        /// <returns></returns>
+        int GetSumWelfare(BonusRecord br);
+        /// <summary>
+        /// 根據 User_id 和 時間 的倒序查詢出相應的列表 抵用券
+        /// </summary>
+        /// <param name="br"></param>
+        /// <returns></returns>
+        List<BonusMaster> GetWelfareByEndTime(BonusRecord br);
+        /// <summary>
+        /// 根據 master_id 修改數據 
+        /// </summary>
+        /// <param name="br"></param>
         /// <returns></returns>
         string UpdateBonusMasterBalance(BonusMaster bm);
-        List<BonusMasterQuery> IsExtendBonus(BonusMasterQuery query);
 
+        List<BonusMasterQuery> IsExtendBonus(BonusMasterQuery query);
         int BonusAmount(BonusMasterQuery query);
         List<BonusMaster> GetBonus(BonusMasterQuery query);
         string UpBonusMaster(BonusMasterQuery query);
