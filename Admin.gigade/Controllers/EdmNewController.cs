345d321215ff664223a04d514ec96d8b516be988
yachao1120j <git@192.168.71.9:testing.git>
2015-09-21 14:17:16 +0800

    添加4個Model  1個控制器 EdmNew
    1 EdmGroupNew
    2 EdmContentNew
    3 Edm_Subscription
    4 Edm_Template

---
+++ Admin.gigade/Controllers/EdmNewController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Admin.gigade.Controllers
+{
+    public class EdmNewController : Controller
+    {
+        //
+        // GET: /EdmNew/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
