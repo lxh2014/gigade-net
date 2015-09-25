59fa2a6259bee905fd466f90039d9cf4cb830370
xiaohui1027j <xiaohui1027j@gimg.tw>
2015-09-25 10:30:42 +0800

    採購服務 邏輯代碼新增

--- Admin.gigade/Controllers/ScheduleServiceController.cs
+++ Admin.gigade/Controllers/ScheduleServiceController.cs
@@ -28,16 +28,58 @@ namespace Admin.gigade.Controllers
         {
             //獲取需要執行的排程
 
-            List<ScheduleServiceQuery> store = new List<ScheduleServiceQuery>();
-            ScheduleServiceQuery query = new ScheduleServiceQuery();
-            query.state = 1;
-
+            List<ScheduleMasterQuery> store = new List<ScheduleMasterQuery>();
             _secheduleServiceMgr = new ScheduleServiceMgr(mySqlConnectionString);
-            
+
             try
             {
+                ScheduleMasterQuery query = new ScheduleMasterQuery();
+                query.state = 1;
+                store = _secheduleServiceMgr.GetExeScheduleMasterList(query);
+                foreach (ScheduleMasterQuery item in store)
+                {
+                    //獲取該排程參數
+                    List<ScheduleConfigQuery> store_config = new List<ScheduleConfigQuery>();
+                    ScheduleConfigQuery query_config = new ScheduleConfigQuery();
+                    query_config.schedule_id = item.rowid;
+                    store_config = _secheduleServiceMgr.GetScheduleConfig(query_config);
+                    
+                    //執行排程
+                    bool result = _secheduleServiceMgr.ExeScheduleService(item.api);
+                    if (result)
+                    {
+                        //記錄排程執行記錄
+                        ScheduleLogQuery query_log = new ScheduleLogQuery();
+                        query_log.schedule_id = item.rowid;
+                        query_log.schedule_period_id = item.schedule_period_id;
+                        query_log.create_user = int.Parse((System.Web.HttpContext.Current.Session["caller"] as BLL.gigade.Model.Caller).user_id.ToString());
 
-                store = _secheduleServiceMgr.GetExeScheduleServiceList(query);
+                        query_log.ipfrom = BLL.gigade.Common.CommonFunction.GetIP4Address(Request.UserHostAddress.ToString());
+                        _secheduleServiceMgr.AddScheduleLog(query_log);
+                    }
+
+                    //更新SchedulePeriod表的current_nums;ScheduleMaster表的previous_execute_time、next_execute_time、state；
+                    //更新current_nums欄位；
+                    SchedulePeriodQuery query_period = new SchedulePeriodQuery();
+                    query_period.rowid = item.schedule_period_id;
+                    query_period = _secheduleServiceMgr.GetSchedulePeriod(query_period);
+                    query_period.current_nums += 1;
+                    _secheduleServiceMgr.UpdateSchedulePeriod(query_period);
+
+                    //更新ScheduleMaster表的previous_execute_time、next_execute_time、state；
+                    item.previous_execute_time = item.next_execute_time;
+                    //獲取next_execute_time和schedule_period_id
+                    int schedule_period_id = 0;
+                    item.next_execute_time = _secheduleServiceMgr.GetNext_Execute_Time(item.rowid, out schedule_period_id);
+                    item.schedule_period_id = schedule_period_id;
+                    if (item.next_execute_time == 0)
+                    {
+                        item.state = 0;
+                    }
+                    _secheduleServiceMgr.UpdateScheduleMaster(item);
+                                        
+
+                }
                 
             }
             catch (Exception ex)
@@ -52,6 +94,7 @@ namespace Admin.gigade.Controllers
             //this.Response.End();
             return this.Response;
         }
+        
 
     }
 }
--- BLL.gigade/Mgr/ScheduleServiceMgr.cs
+++ BLL.gigade/Mgr/ScheduleServiceMgr.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using BLL.gigade.Dao;
 using BLL.gigade.Model.Query;
@@ -23,17 +24,228 @@ namespace BLL.gigade.Mgr
             }
 
         }
-        public List<ScheduleServiceQuery> GetExeScheduleServiceList(ScheduleServiceQuery query)
+        public List<ScheduleMasterQuery> GetExeScheduleMasterList(ScheduleMasterQuery query)
         {
             try
             {
-                return _secheduleServiceDao.GetExeScheduleServiceList(query);
+                return _secheduleServiceDao.GetExeScheduleMasterList(query);
             }
             catch (Exception ex)
             {
 
-                throw new Exception("SecheduleServiceMgr-->GetExeScheduleServiceList-->" + ex.Message, ex);
+                throw new Exception("SecheduleServiceMgr-->GetExeScheduleMasterList-->" + ex.Message, ex);
             }
         }
+        public List<ScheduleConfigQuery> GetScheduleConfig(ScheduleConfigQuery query)
+        {
+            try
+            {
+                return _secheduleServiceDao.GetScheduleConfig(query);
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception("SecheduleServiceMgr-->GetScheduleConfig-->" + ex.Message, ex);
+            }
+        }
+        public SchedulePeriodQuery GetSchedulePeriod(SchedulePeriodQuery query)
+        {
+            try
+            {
+                return _secheduleServiceDao.GetSchedulePeriod(query);
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception("SecheduleServiceMgr-->GetSchedulePeriod-->" + ex.Message, ex);
+            }
+        }
+
+
+        public int UpdateScheduleMaster(ScheduleMasterQuery query)
+        {
+            try
+            {
+                return _secheduleServiceDao.UpdateScheduleMaster(query);
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception("SecheduleServiceMgr-->UpdateScheduleMaster-->" + ex.Message, ex);
+            }
+        }
+        public int UpdateSchedulePeriod(SchedulePeriodQuery query)
+        {
+            try
+            {
+                return _secheduleServiceDao.UpdateSchedulePeriod(query);
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception("SecheduleServiceMgr-->UpdateSchedulePeriod-->" + ex.Message, ex);
+            }
+        }
+        public bool ExeScheduleService(string api)
+        {
+            bool result = false;
+            try
+            {
+                if(!string.IsNullOrEmpty(api))
+                {
+                    //api=System.Net.Dns.GetHostAddresses(System.Net.Dns.GetHostName())[2].ToString()+"/"+api;
+                    api = "http://localhost:11804/" + api;
+
+                    HttpWebRequest httpRequest = (HttpWebRequest)WebRequest.Create(api);
+                    httpRequest.Timeout = 2000;
+                    httpRequest.Method = "GET";
+                    HttpWebResponse httpResponse = (HttpWebResponse)httpRequest.GetResponse();
+
+                    result = true;
+                }
+                return result;
+
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception("SecheduleServiceMgr-->ExeScheduleService-->" + ex.Message, ex);
+            }
+        }
+        public int AddScheduleLog(ScheduleLogQuery query)
+        {
+            try
+            {
+                return _secheduleServiceDao.AddScheduleLog(query);
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception("SecheduleServiceMgr-->AddScheduleLog-->" + ex.Message, ex);
+            }
+        }
+        public int GetNext_Execute_Time(int schedule_id, out int schedule_period_id)
+        {
+            try
+            {
+                int time = 0;
+                schedule_period_id = 0;
+                List<SchedulePeriodQuery> store = _secheduleServiceDao.GetSchedulePeriodBySchedule(schedule_id);
+                foreach(SchedulePeriodQuery item in store)
+                {
+                    if (item.current_nums >= item.limit_nums)
+                    {
+                        continue;
+                    }
+                    int begin_datetime = item.begin_datetime;
+                    int now = (int)Common.CommonFunction.GetPHPTime();
+                    if (item.period_type=="year")
+                    {
+                        if (item.year > 0)
+                        {
+                            while (begin_datetime < now)
+                            {
+                                begin_datetime = (int)Common.CommonFunction.GetPHPTime(Common.CommonFunction.GetNetTime(begin_datetime).AddMonths(item.year).ToString());
+                            }
+                            if (time > begin_datetime || time==0)
+                            {
+                                time = begin_datetime;
+                                schedule_period_id = item.rowid;
+                            }
+                        }
+                        
+                    }
+                    else if (item.period_type=="month")
+                    {
+                        if (item.month > 0)
+                        {
+                            while (begin_datetime < now)
+                            {
+                                    begin_datetime = (int)Common.CommonFunction.GetPHPTime(Common.CommonFunction.GetNetTime(begin_datetime).AddMonths(item.month).ToString());
+                                
+                            }
+                            if (time > begin_datetime || time == 0)
+                            {
+                                time = begin_datetime;
+                                schedule_period_id = item.rowid;
+                            }
+                        }
+                    }
+                    else if (item.period_type=="week")
+                    {
+                        if (item.week > 0)
+                        {
+                            while (begin_datetime < now)
+                            {
+                                    begin_datetime = (int)Common.CommonFunction.GetPHPTime(Common.CommonFunction.GetNetTime(begin_datetime).AddDays(item.week*7).ToString());
+                               
+                            }
+                            if (time > begin_datetime || time == 0)
+                            {
+                                time = begin_datetime;
+                                schedule_period_id = item.rowid;
+                            }
+                        }
+
+                    }
+                    else if (item.period_type=="day")
+                    {
+                        if (item.day > 0)
+                        {
+                            while (begin_datetime < now)
+                            {
+                                    begin_datetime = (int)Common.CommonFunction.GetPHPTime(Common.CommonFunction.GetNetTime(begin_datetime).AddDays(item.day).ToString());
+                                
+                            }
+                            if (time > begin_datetime || time == 0)
+                            {
+                                time = begin_datetime;
+                                schedule_period_id = item.rowid;
+                            }
+                        }
+                    }
+                    else if (item.period_type=="hour")
+                    {
+                        if (item.hour > 0)
+                        {
+                            while (begin_datetime < now)
+                            {
+                                    begin_datetime = (int)Common.CommonFunction.GetPHPTime(Common.CommonFunction.GetNetTime(begin_datetime).AddHours(item.hour).ToString());
+                                
+                            }
+                            if (time > begin_datetime || time == 0)
+                            {
+                                time = begin_datetime;
+                                schedule_period_id = item.rowid;
+                            }
+                        }
+                    }
+                    else if (item.period_type=="minute")
+                    {
+                        if (item.minute > 0)
+                        {
+                            while (begin_datetime < now)
+                            {
+                                    begin_datetime = (int)Common.CommonFunction.GetPHPTime(Common.CommonFunction.GetNetTime(begin_datetime).AddMinutes(item.minute).ToString());
+                                
+                            }
+                            if (time > begin_datetime || time == 0)
+                            {
+                                time = begin_datetime;
+                                schedule_period_id = item.rowid;
+                            }
+                        }
+                    }
+                    
+                }
+                return time;
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception("SecheduleServiceMgr-->GetNext_Execute_Time-->" + ex.Message, ex);
+            }
+        }
+
     }
 }
