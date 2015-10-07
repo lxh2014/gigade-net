9abad1ce9f2a9b77da1726b67209d6135b83d55d
jiaohe0625j <jiaohe0625j@gimg.tw>
2015-10-07 10:33:45 +0800

    電子報列表、活動頁面列表新增編輯完成之後刷新列表頁

---
+++ BLL.gigade/Mgr/TFunctionMgr.cs
@@ -0,0 +1,30 @@
+﻿using BLL.gigade.Dao;
+using BLL.gigade.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+
+namespace BLL.gigade.Mgr
+{
+    public class TFunctionMgr
+    {
+        private TFunctionDao _tFunctionDao;
+        public TFunctionMgr(string connectionStr)
+        {
+            _tFunctionDao = new TFunctionDao(connectionStr);
+        }
+        public DataTable GetModel(TFunction query)
+        {
+            try
+            {
+                return _tFunctionDao.GetModel(query);
+            }
+            catch (Exception ex)
+            {              
+              throw new Exception("TFunctionMgr-->GetModel-->" + ex.Message, ex);
+            }
+        }
+    }
+}
