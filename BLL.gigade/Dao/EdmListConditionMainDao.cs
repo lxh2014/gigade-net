499104cacce97b2498b27b74605fe5ca409c2987
jialei0706h <jialei0706h@gimg.tw>
2015-11-12 14:05:07 +0800

    優化sql
    FIX：
    優化名單篩選sql

--- BLL.gigade/Dao/EdmListConditionMainDao.cs
+++ BLL.gigade/Dao/EdmListConditionMainDao.cs
@@ -139,7 +139,7 @@ namespace BLL.gigade.Dao
                 }
                 if (q.ChkAge)
                 {//年齡
-                    where.AppendFormat(" AND u.user_birthday_year BETWEEN {0} AND {1} ", DateTime.Now.Year - q.ageMin, DateTime.Now.Year - q.ageMax);
+                    where.AppendFormat(" AND u.user_birthday_year BETWEEN {0} AND {1} ", DateTime.Now.Year - q.ageMax, DateTime.Now.Year - q.ageMin);
                 }
                 if (q.ChkCancel)
                 {//取消次數
