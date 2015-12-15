3af0caf71ad4861dadc7144eebe03a04749f8a26
hongfei0416j <hongfei0416j@gimg.tw>
2015-12-15 14:56:43 +0800

    api單元測試及相關model
    Add：
    1、新增api測試及model

---
+++ BLL.gigade/Common/EncryptComputer.cs
@@ -0,0 +1,145 @@
+﻿using BLL.gigade.Mgr;
+using BLL.gigade.Model.APIModels;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+
+namespace BLL.gigade.Common
+{
+    public static class EncryptComputer
+    {
+
+        private static byte[] _salt = Encoding.ASCII.GetBytes("4159eecb1277e4d79f8402edae447b");
+        private static string _secret = "e719f4b9a693072e41b5295e867f4b";
+
+        #region AES加密 +  string EncryptStringAES(string plainText)
+        /// <summary>
+        /// AES加密
+        /// </summary>
+        /// <param name="plainText">要加密的字串</param>
+        /// <returns>加密后的字串</returns>
+        public static string EncryptStringAES(string plainText)
+        {
+            if (string.IsNullOrEmpty(plainText))
+                throw new ArgumentNullException("plainText");
+
+            string outStr = null;
+            RijndaelManaged aesAlg = null;
+
+            try
+            {
+                Rfc2898DeriveBytes key = new Rfc2898DeriveBytes(_secret, _salt);
+                aesAlg = new RijndaelManaged();
+                aesAlg.Key = key.GetBytes(aesAlg.KeySize / 8);
+                ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+                using (MemoryStream msEncrypt = new MemoryStream())
+                {
+                    msEncrypt.Write(BitConverter.GetBytes(aesAlg.IV.Length), 0, sizeof(int));
+                    msEncrypt.Write(aesAlg.IV, 0, aesAlg.IV.Length);
+                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                        {
+                            swEncrypt.Write(plainText);
+                        }
+                    }
+                    outStr = Convert.ToBase64String(msEncrypt.ToArray());
+                }
+            }
+            finally
+            {
+                if (aesAlg != null)
+                    aesAlg.Clear();
+            }
+            return outStr;
+        } 
+        #endregion
+
+        #region AES解密 +string DecryptStringAES(string cipherText)
+        /// <summary>
+        /// AES解密
+        /// </summary>
+        /// <param name="cipherText">要解密的字串</param>
+        /// <returns>解密后的字串</returns>
+        public static string DecryptStringAES(string cipherText)
+        {
+            if (string.IsNullOrEmpty(cipherText))
+                throw new ArgumentNullException("cipherText");
+            RijndaelManaged aesAlg = null;
+            string plaintext = null;
+
+            try
+            {
+                Rfc2898DeriveBytes key = new Rfc2898DeriveBytes(_secret, _salt);
+                byte[] bytes = Convert.FromBase64String(cipherText);
+                using (MemoryStream msDecrypt = new MemoryStream(bytes))
+                {
+                    aesAlg = new RijndaelManaged();
+                    aesAlg.Key = key.GetBytes(aesAlg.KeySize / 8);
+                    aesAlg.IV = ReadByteArray(msDecrypt);
+                    ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                            plaintext = srDecrypt.ReadToEnd();
+                    }
+                }
+            }
+            finally
+            {
+                if (aesAlg != null)
+                    aesAlg.Clear();
+            }
+
+            return plaintext;
+        } 
+       
+
+        private static byte[] ReadByteArray(Stream s)
+        {
+            byte[] rawLength = new byte[sizeof(int)];
+            if (s.Read(rawLength, 0, rawLength.Length) != rawLength.Length)
+            {
+                throw new SystemException("Stream did not contain properly formatted byte array");
+            }
+
+            byte[] buffer = new byte[BitConverter.ToInt32(rawLength, 0)];
+            if (s.Read(buffer, 0, buffer.Length) != buffer.Length)
+            {
+                throw new SystemException("Did not read byte array properly");
+            }
+
+            return buffer;
+        }
+        #endregion
+
+        public static List<AesResult> EncryptListByApi(List<PlainTextViewModel> plainText)
+        {
+
+            GigadeApiRequest request = new GigadeApiRequest();
+            var result = request.Request<List<PlainTextViewModel>, List<AesResult>>("api/Utility/EncryptWithAes", plainText);
+            if (result.success)
+            {
+                return result.result;
+            }
+            else
+            {
+                return null;
+            }
+            
+        }
+
+        public static string DecryptByApi(string plainText)
+        {
+
+            GigadeApiRequest request = new GigadeApiRequest();
+            var result = request.Request<PlainTextViewModel, AesResult>("api/Utility/DecryptWithAes", new PlainTextViewModel { text = plainText });
+            return result.result.computed_text;
+        }
+    }
+}
