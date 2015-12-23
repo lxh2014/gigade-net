ec6ab8731105dfd26eddcfa8e63e2bdf0924bda4
chaojie1124j <chaojie1124j@gimg.tw>
2015-12-23 10:01:19 +0800

    更改合併衝突
    FIX:
    獲取IpoNvdLog.cs出錯，原因是可能別人提交的時候對這個類添加了多個重複的字段。

--- BLL.gigade/Model/IpoNvdLog.cs
+++ BLL.gigade/Model/IpoNvdLog.cs
@@ -10,12 +10,9 @@ namespace BLL.gigade.Model
         public int row_id { get; set; }
         public string work_id {get;set;}
         public string ipo_id {get;set;}
-        public int item_id {get;set;}
         public int add_qty {get;set;}
         public DateTime cde_date {get;set;}
         public uint item_id {get;set;}
-        public int add_qty {get;set;}
-        public DateTime cde_date {get;set;}
         public DateTime made_date {get;set;}
         public int create_user {get;set;}
         public DateTime create_datetime {get;set;}
