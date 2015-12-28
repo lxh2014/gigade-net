271f6da74c6fb116741f42896000dbe6ca803c2e
chaojie1124j <chaojie1124j@gimg.tw>
2015-12-28 15:08:05 +0800

    更改系統排程服務和料位管理
    FIX:
    1.出貨單期望到貨日排程的更改標註顏色
    2.總量檢貨報表的更改,取規格

--- BLL.gigade/Mgr/Impl/IDeliverChangeLogImplMgr.cs
+++ BLL.gigade/Mgr/Impl/IDeliverChangeLogImplMgr.cs
@@ -14,7 +14,7 @@ namespace BLL.gigade.Mgr.Impl
         int insertDeliverChangeLog(DeliverChangeLog dCL);
         List<DeliverChangeLogQuery> GetDeliverChangeLogList(DeliverChangeLogQuery Query, out int totalCount);
         bool Start(string schedule_code);
-        string GetHtmlByDataTable(DataTable _dtmyMonth);
+        string GetHtmlByDataTable(DataTable _dtmyMonth,string DeleteName=null,string value=null);
         bool isCanModifyExpertArriveDate(string apiServer, long deliver_id);
         bool ModifyExpertArriveDate(string apiServer, ModifyExpertArriveDateViewModel expertArriveDateViewModel);
     }
